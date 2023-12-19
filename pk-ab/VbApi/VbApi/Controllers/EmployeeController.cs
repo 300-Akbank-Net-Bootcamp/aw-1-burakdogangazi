5a04f1e73ef92eb4a25f24e02b7646f7c34f58e2
Burak Doğan <burak.dogan1@huawei.com>
2023-12-19 16:25:37 +0300

    [TicketNo:] REQ-1 [Description:] Implementing Fluent Validation to Employee And Staff Models in Controller [Binary Source:] NA

--- pk-ab/VbApi/VbApi/Controllers/EmployeeController.cs
+++ pk-ab/VbApi/VbApi/Controllers/EmployeeController.cs
@@ -1,60 +1,15 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using VbApi.ValidationRules;
 
 namespace VbApi.Controllers;
 
-public class Employee : IValidatableObject
+public class Employee
 {
-    [Required]
-    [StringLength(maximumLength: 250, MinimumLength = 10, ErrorMessage = "Invalid Name")]
     public string Name { get; set; }
-
-    [Required] 
     public DateTime DateOfBirth { get; set; }
-
-    [EmailAddress(ErrorMessage = "Email address is not valid.")]
-    public string Email { get; set; }
-
-    [Phone(ErrorMessage = "Phone is not valid.")]
-    public string Phone { get; set; }
-
-    [Range(minimum: 50, maximum: 400, ErrorMessage = "Hourly salary does not fall within allowed range.")]
-    [MinLegalSalaryRequired(minJuniorSalary: 50, minSeniorSalary: 200)]
-    public double HourlySalary { get; set; }
-
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-    {
-        var minAllowedBirthDate = DateTime.Today.AddYears(-65);
-        if (minAllowedBirthDate > DateOfBirth)
-        {
-            yield return new ValidationResult("Birthdate is not valid.");
-        }
-    }
-}
-
-public class MinLegalSalaryRequiredAttribute : ValidationAttribute
-{
-    public MinLegalSalaryRequiredAttribute(double minJuniorSalary, double minSeniorSalary)
-    {
-        MinJuniorSalary = minJuniorSalary;
-        MinSeniorSalary = minSeniorSalary;
-    }
-
-    public double MinJuniorSalary { get; }
-    public double MinSeniorSalary { get; }
-    public string GetErrorMessage() => $"Minimum hourly salary is not valid.";
-
-    protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
-    {
-        var employee = (Employee)validationContext.ObjectInstance;
-        var dateBeforeThirtyYears = DateTime.Today.AddYears(-30);
-        var isOlderThanThirdyYears = employee.DateOfBirth <= dateBeforeThirtyYears;
-        var hourlySalary = (double)value;
-
-        var isValidSalary = isOlderThanThirdyYears ? hourlySalary >= MinSeniorSalary : hourlySalary >= MinJuniorSalary;
-
-        return isValidSalary ? ValidationResult.Success : new ValidationResult(GetErrorMessage());
-    }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public double? HourlySalary { get; set; }
 }
 
 [Route("api/[controller]")]
@@ -66,12 +21,17 @@ public class EmployeeController : ControllerBase
     }
 
     [HttpPost]
-    public Employee Post([FromBody] Employee value)
+    public IActionResult Post([FromBody] Employee value)
     {
-        if (value.DateOfBirth > DateTime.Now.AddYears(-30) && value.HourlySalary < 200)
+        EmployeeValidator employeeValidator = new EmployeeValidator();
+
+        var validationResult = employeeValidator.Validate(value);
+
+        if (!validationResult.IsValid)
         {
-            
+            return BadRequest(validationResult.Errors);
         }
-        return value;
+
+        return Ok(value);
     }
 }
--- pk-ab/VbApi/VbApi/Controllers/StaffController.cs
+++ pk-ab/VbApi/VbApi/Controllers/StaffController.cs
@@ -1,21 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
+using VbApi.ValidationRules;
 
 namespace VbApi.Controllers;
 
 public class Staff
 {
-    [Required]
-    [StringLength(maximumLength: 250, MinimumLength = 10)]
     public string? Name { get; set; }
 
-    [EmailAddress(ErrorMessage = "Email address is not valid.")]
     public string? Email { get; set; }
 
-    [Phone(ErrorMessage = "Phone is not valid.")]
     public string? Phone { get; set; }
 
-    [Range(minimum: 30, maximum: 400, ErrorMessage = "Hourly salary does not fall within allowed range.")]
     public decimal? HourlySalary { get; set; }
 }
 
@@ -23,13 +20,18 @@ public class Staff
 [ApiController]
 public class StaffController : ControllerBase
 {
-    public StaffController()
-    {
-    }
-
     [HttpPost]
-    public Staff Post([FromBody] Staff value)
+    public IActionResult Post([FromBody] Staff value)
     {
-        return value;
+        StaffValidator staffValidator = new StaffValidator();
+
+        var validationResult = staffValidator.Validate(value);
+
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.Errors);
+        }
+
+        return Ok(value);
     }
 }
